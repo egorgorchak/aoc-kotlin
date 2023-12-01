b1f3911440785c5c19f6fe14cf7ab78a1a88cde9
egorlaptev <egor.gorchak@gmail.com>
2023-12-01 20:37:27 +0200

    Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,37 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var result = 0
+        for (line in input) {
+            val numbers = line.filter { it.isDigit() }
+            val first = "${numbers[0]}${numbers[numbers.length - 1]}"
+
+            result += first.toInt()
+        }
+        return result
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
-    }
+        val toRegex = "(one|two|three|four|five|six|seven|eight|nine)".toRegex()
+        var result = 0
+        for (line in input) {
+            var dup = line
+            while (toRegex.find(dup) != null) {
+                val matchResult = toRegex.find(dup)
+                dup = dup.replaceRange(
+                    matchResult!!.range.first,
+                    matchResult.range.first + 1,
+                    numbersAsStrings[matchResult.value].toString()
+                )
+                matchResult.next()
+            }
+            val numbers = dup.filter { it.isDigit() }
+            val first = "${numbers[0]}${numbers[numbers.length - 1]}"
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+            result += first.toInt()
+        }
+
+        return result
+    }
 
     val input = readInput("Day01")
     part1(input).println()
--- src/Utils.kt
+++ src/Utils.kt
@@ -19,3 +19,14 @@ fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteA
  * The cleaner shorthand for printing output.
  */
 fun Any?.println() = println(this)
+
+val numbersAsStrings = mapOf(
+    "one" to 1,
+    "two" to 2,
+    "three" to 3,
+    "four" to 4,
+    "five" to 5,
+    "six" to 6,
+    "seven" to 7,
+    "eight" to 8,
+    "nine" to 9)
