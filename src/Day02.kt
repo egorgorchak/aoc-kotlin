d412d768f3a5dacb0d4778136e867d3fb2ea2af1
egorlaptev <egor.gorchak@gmail.com>
2023-12-02 13:54:25 +0200

    Day02

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,13 +1,16 @@
 import kotlin.math.absoluteValue
 
+const val RED = "red"
+const val GREEN = "green"
+const val BLUE = "blue"
+
 fun main() {
     fun part1(input: List<String>): Int {
         var gameId = 0
-
         for (line in input) {
-            val split = line.split(":", ";")
-            val currentGameId = split[0].filter { it.isDigit() }.toInt()
-            if (isGamePossible(split.drop(1))) {
+            val splitLines = line.split(":", ";")
+            if (isGamePossible(splitLines.drop(1))) {
+                val currentGameId = splitLines[0].filter { it.isDigit() }.toInt()
                 gameId += currentGameId
             }
         }
@@ -26,65 +29,53 @@ fun main() {
         return gameId
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day02_test")
-    val part2 = part2(testInput)
-    println(part2)
-    check(part2 == 2286)
-
     val input = readInput("Day02")
     part1(input).println()
     part2(input).println()
 }
 
 fun isGamePossible(lines: List<String>): Boolean {
-    val red = 12
-    val green = 13
-    val blue = 14
-    var gameId = true
+    var isGamePossible = true
 
     for (element in lines) {
-        val map = mutableMapOf("red" to 0, "green" to 0, "blue" to 0)
-        val filter = element.split(",", " ").filter { it.isNotBlank() }
+        val cubeCount = mutableMapOf(RED to 0, GREEN to 0, BLUE to 0)
+        val game = element.split(",", " ").filter { it.isNotBlank() }
 
-        for (i in filter.indices step 2) {
-            var a = map[filter[i + 1]]
+        for (i in game.indices step 2) {
+            var cubes = cubeCount[game[i + 1]]
 
-            a = a?.plus(filter[i].toInt())
-            if (a != null) {
-                map[filter[i + 1]] = a
+            cubes = cubes?.plus(game[i].toInt())
+            if (cubes != null) {
+                cubeCount[game[i + 1]] = cubes
             }
         }
-        val currentRed = map["red"]
-        val currentGreen = map["green"]
-        val currentBlue = map["blue"]
 
-        if (currentRed?.absoluteValue!! > red
-            || currentGreen?.absoluteValue!! > green
-            || currentBlue?.absoluteValue!! > blue
+        if (cubeCount[RED]?.absoluteValue!! > 12
+            || cubeCount[GREEN]?.absoluteValue!! > 13
+            || cubeCount[BLUE]?.absoluteValue!! > 14
         ) {
-            gameId = false
+            isGamePossible = false
             break
         }
     }
 
-    return gameId
+    return isGamePossible
 }
 
 fun neededCubes(lines: List<String>): Int {
-    val map = mutableMapOf("red" to 0, "green" to 0, "blue" to 0)
+    val cubeCount = mutableMapOf(RED to 0, GREEN to 0, BLUE to 0)
     for (element in lines) {
-        val filter = element.split(",", " ").filter { it.isNotBlank() }
+        val game = element.split(",", " ").filter { it.isNotBlank() }
 
-        for (i in filter.indices step 2) {
-            val s = filter[i + 1]
-            val s1 = filter[i].toInt()
+        for (i in game.indices step 2) {
+            val color = game[i + 1]
+            val quantity = game[i].toInt()
 
-            if (map[s]?.absoluteValue!! < s1) {
-                map[s] = s1
+            if (cubeCount[color]?.absoluteValue!! < quantity) {
+                cubeCount[color] = quantity
             }
         }
     }
 
-    return map["red"]?.absoluteValue!! * map["green"]?.absoluteValue!! * map["blue"]?.absoluteValue!!
+    return cubeCount[RED]?.absoluteValue!! * cubeCount[GREEN]?.absoluteValue!! * cubeCount[BLUE]?.absoluteValue!!
 }
